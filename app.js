a7a4adc32c478fa7442dafdcf61906192121e66f
Wade Dorrell <wade@dorrells.org>
2015-12-06 12:48:17 -0700

    Add test ios banner thing

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ app.get('/', function(req, res) {
 	res.end(Math.random() + '');
 });
 
+app.get('/iosbanner', function(req, res) {
+	res.write('<html><body>iosbanner</body></html>');	
+});
+
 app.listen(process.env.PORT || 8888);
